0e54161bc9e538ed5f78ba83e08713835d77a399
kkk293590509 <k293590509@gmail.com>
2017-04-07 10:02:48 +0800

    implement post create funtion

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,23 @@
 
-<h2 class="text-center">新增文章</h2>
-
-<div class="col-md-4 col-md-offset-4">
-  <%= simple_form_for [@group,@post] do |f| %>
-    <div class="form-group">
-      <%= f.input :content, input_html: { class: "form-control"} %>
-    </div>
-    <div class="form-actions">
-      <%= f.submit "Submit", disable_with: "Submiting...", class: "btn btn-primary"%>
-    </div>
-  <% end %>
-</div>
+class PostsController < ApplicationController
+  before_action :authenticate_user!, :only => [:new, :create]
+  def new
+    @group = Group.find(params[:group_id])
+    @post =Post.new
+  end
+  def create
+    @group =Group.find(params[:group_id])
+    @post =Post.new(post_params)
+    @post.group =@group
+    @post.user= current_user
+      if @post.save
+      redirect_to group_path(@group)
+      else
+      render :new
+      end
+   end
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
+end
